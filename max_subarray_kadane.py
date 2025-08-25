c7a25768445063a50563be22b9bdbb5130ec7505
Saketh Reddy Regatte <sakethreddy1305@gmail.com>
2025-08-24 23:51:27 -0700

    Algo : KADANE Algorithm

---
+++ max_subarray_kadane.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def max_sum(nums : List):
+        max_sum = float('-inf')
+        curr_sum = 0
+        n = len(nums)
+        
+        for i in range(n):
+            curr_sum += nums[i]
+            max_sum = max(max_sum, curr_sum)
+            if curr_sum < 0:
+                curr_sum = 0
+        
+        return max_sum
